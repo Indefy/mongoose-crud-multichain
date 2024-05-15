eec6840dc939076da6fca4b96fb3f30f3d0b2af7
Adi Booker <adib4324@gmail.com>
2024-05-15 20:24:16 +0300

    first commit

---
+++ server/db/mongoose.connection.mjs
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+import log from "@ajar/marker";
+
+export async function connect_db(uri) {
+	await mongoose.connect(uri);
+	log.magenta(" ✨  Connected to Mongo DB ✨ ");
+}
